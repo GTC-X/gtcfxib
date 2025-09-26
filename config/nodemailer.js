03cbda868648aa26438796d7ee6f0b56f2ba5466
webteamgtc <144129216+webteamgtc@users.noreply.github.com>
2025-09-26 13:30:35 +0400

    dsdsddsds

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -1,23 +1,30 @@
 import nodemailer from 'nodemailer';
 
 export const transporter = nodemailer.createTransport({
-  host: 'smtp.eu.mailgun.org', // Mailgun SMTP host
+  host: 'smtpdm-eu-central-1.aliyuncs.com', // Mailgun SMTP host
   port: 465,                   // SSL/TLS port
   secure: true,                // Use TLS
   auth: {
-    user: 'portal@mx1.gtcmail.com', // Mailgun SMTP username
-    pass: '48490f5c91f63ae6e470633d79aa10fb-7c5e3295-a24fb77e', // Mailgun SMTP password
+    user: 'portal@mx4.gtcmail.com', // Mailgun SMTP username
+    pass: 'Ab3Cde4FgH', // Mailgun SMTP password
   },
 });
 
 export const mailOptions = {
-  from: 'portal@mx1.gtcmail.com',
-  to: 'support@gtcfxiq.com, support@gtcup.com, marginbonus@gtcfx.com',
+  from: 'portal@mx4.gtcmail.com',
+  to: 'support@gtcfxiq.com',
   bcc: 'mohammad.zeeshan@gtcfx.com',
 };
 
 export const mailOptionsJobs = {
-  from: 'portal@mx1.gtcmail.com',
+  from: 'portal@mx4.gtcmail.com',
   to: 'careers@gtcfx.com',
   bcc: 'zeeshan@gtcfx.com',
 };
+
+export const mailOptionsSupports = {
+  from: 'portal@mx4.gtcmail.com',
+  to: 'support@gtcfx.com',
+  bcc: 'zeeshan@gtcfx.com',
+};
+ 
